a1df8c4b9d1baf279d58bf37a6449c13763dc457
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-21 18:15:22 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(record){
+    if(record[0].year){
+        let b = record.find(o => o.result === 'W');
+        return b.year
+    }else{
+        return record.find(c => c.result ==="W")
+    }
+}
+
+
+
 // code your solution here
